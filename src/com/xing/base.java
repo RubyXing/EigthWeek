90e1e6468c7bf14eb8c979398115b1deedc14219
RubyXing <593680319@qq.com>
2020-05-14 11:56:36 +0800

    commit from class PC

--- src/com/xing/base.java
+++ src/com/xing/base.java
@@ -3,6 +3,7 @@ package com.xing;/*
  *
  */
 
+import lombok.Cleanup;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -12,21 +13,30 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+
 public class base {
     public static void main(String[] args) {
         try {
-            InputStream stream = Resources.getResourceAsStream("baits-config.xml");
+            @Cleanup InputStream stream = Resources.getResourceAsStream("baits-config.xml");
 
             SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(stream);
 
-            SqlSession session = factory.openSession();
+            @Cleanup SqlSession session = factory.openSession();
 
             List<Dept> list= session.selectList("abc.all");
 
             System.out.println(list);
 
+            Dept dept=new Dept().setDeptno(12).setDname("幸琦林").setLoc("NewCity");
+            //mybatis操作数据库需要手动提交
+            session.insert("abc.add",dept);
+
+            session.commit();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
     }
 }
